102aa1ef1db74a8f2ec09823aa9aab7ba5564bc6
kamrul-hasan01 <k17h02@gmail.com>
2022-04-26 14:59:23 +0600

    page layout setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Layout from "../components/layout/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>site</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
