42452e305612b9221fba3a58cbbf2e1f85582824
kamrul-hasan01 <k17h02@gmail.com>
2022-04-28 16:05:29 +0600

    clean

--- Firebase/Firebase.init.js
+++
@@ -1,6 +0,0 @@
-import firebaseConfig from "./Firebase.confiq";
-import { initializeApp } from "firebase/app";
-const initializeAuthentication = () => {
-  initializeApp(firebaseConfig);
-};
-export default initializeAuthentication;
