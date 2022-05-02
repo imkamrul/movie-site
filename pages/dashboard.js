2de0ce3055da67389821f48b686e9e9650b3e5be
kamrul-hasan01 <k17h02@gmail.com>
2022-05-02 14:11:09 +0600

    done

--- pages/dashboard.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DashBoardLayout from "../components/layout/DashBoardLayout";
-
-const Dashboard = () => {
-  return <DashBoardLayout />;
-};
-
-export default Dashboard;
