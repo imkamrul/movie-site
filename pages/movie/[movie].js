64ffb2eb6767c2e82ce42041d565b4c0098986ac
kamrul-hasan01 <k17h02@gmail.com>
2022-05-16 12:23:00 +0600

    done

--- pages/movie/[movie].js
+++ pages/movie/[movie].js
@@ -10,14 +10,14 @@ const Movie = ({ data }) => {
   );
 };
 
-export async function getStaticPaths() {
-  const res = await fetch("https://kamrul-hasan01.github.io/api/pages.json");
-  const data = await res.json();
-  const paths = data.movie_id.map((item) => ({
-    params: { movie: item.link },
-  }));
-  return { paths, fallback: false };
-}
+// export async function getStaticPaths() {
+//   const res = await fetch("https://kamrul-hasan01.github.io/api/pages.json");
+//   const data = await res.json();
+//   const paths = data.movie_id.map((item) => ({
+//     params: { movie: item.link },
+//   }));
+//   return { paths, fallback: false };
+// }
 
 export async function getServerSideProps({ params }) {
   console.log(params);
